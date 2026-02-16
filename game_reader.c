3225094a965fe7b663feead9f35e64c729e998df
Rafael Velasco Wagner <rafmenthe52@gmail.com>
2026-02-16 21:43:54 +0100

    game_reader update

--- game_reader.c
+++ game_reader.c
@@ -27,160 +27,198 @@ Status game_reader_load_spaces(Game *game, char *filename) {
   char name[WORD_SIZE] = "";
   char *toks = NULL;
   Id id = NO_ID, north = NO_ID, east = NO_ID, south = NO_ID, west = NO_ID;
+  Bool bool_object = FALSE;
   Space *space = NULL;
   Status status = OK;
-  Bool bool_object;
 
-  if (!filename) {
+  /* Control error */
+  if (!game || !filename) {
     return ERROR;
   }
 
-  file = fopen(filename, "r");
-  if (file == NULL) {
+  /* Opens file in read mode */
+  if (!(file = fopen(filename, "r"))) {
     return ERROR;
   }
 
+  /* Reads file line by line */
   while (fgets(line, WORD_SIZE, file)) {
+
+    /* Checks if the line defines a space */
     if (strncmp("#s:", line, 3) == 0) {
+
+      /* Divides space id */
       toks = strtok(line + 3, "|");
       id = atol(toks);
+
+      /* Divides space name */
       toks = strtok(NULL, "|");
+      if (!toks) continue;
       strcpy(name, toks);
+
+      /* Divides connections (north, east, south, west) */
       toks = strtok(NULL, "|");
+      if (!toks) continue;
       north = atol(toks);
+
       toks = strtok(NULL, "|");
+      if (!toks) continue;
       east = atol(toks);
+
       toks = strtok(NULL, "|");
+      if (!toks) continue;
       south = atol(toks);
+
       toks = strtok(NULL, "|");
+      if (!toks) continue;
       west = atol(toks);
+
+      /* Divides object presence flag */
       toks = strtok(NULL, "|");
-      if (toks == 0) {
-        bool_object = FALSE;
-      } else {
-        bool_object = TRUE;
-      }
-#ifdef DEBUG
-      printf("Leido: s:%ld|%s|%ld|%ld|%ld|%ld\n", id, name, north, east, south, west);
-#endif
+      if (!toks) continue;
+      bool_object = atol(toks) ? TRUE : FALSE;
+
+      /* Creates the space */
       space = space_create(id);
       if (!space) {
+        fclose(file);
         return ERROR;
-      }else{  
-        space_set_name(space, name);
-        space_set_north(space, north);
-        space_set_east(space, east);
-        space_set_south(space, south);
-        space_set_west(space, west);
-        space_set_object(space, bool_object);
-        status=game_add_space(game, space);
       }
+
+      /* Sets the space attributes */
+      space_set_name(space, name);
+      space_set_north(space, north);
+      space_set_east(space, east);
+      space_set_south(space, south);
+      space_set_west(space, west);
+      space_set_object(space, bool_object);
+      
+      /* Adds the space to the game */
+      status = game_add_space(game, space);
     }
   }
 
+  /* Checks for reading errors */
   if (ferror(file)) {
     status = ERROR;
   }
 
   fclose(file);
-
   return status;
 }
 
-Status game_reader_load_objects(Game* game, char* filename){
+Status game_reader_load_objects(Game* game, char* filename) {
   FILE *file = NULL;
   char line[WORD_SIZE];
-  char name[WORD_SIZE] = "";
+  char name[WORD_SIZE];
   char *toks = NULL;
   Id id = NO_ID, location = NO_ID;
   Object *object = NULL;
   Space *space = NULL;
   Status status = OK;
 
-  if(!(filename)){
+  /* Control error */
+  if (!game || !filename) {
     return ERROR;
   }
 
-  if(!(file=fopen(filename, "r"))){
+  /* Opens the file */
+  if (!(file = fopen(filename, "r"))) {
     return ERROR;
   }
+  
+  /* Reads the file line by line */
+  while (fgets(line, WORD_SIZE, file)) {
 
-  /*get the variables separated | and copy them to temporary placeholders*/
-  while(fgets(line, WORD_SIZE, file)){
-    if(strncmp("#o:", line, 3) == 0){
-      id=atol(strtok(line+3, "|"));
+    /* Checks if the line defines an object */
+    if (strncmp("#o:", line, 3) == 0) {
+
+      /* Divides the object id */
+      toks = strtok(line + 3, "|");
+      id = atol(toks);
+
+      /* Divides the object name */
       toks = strtok(NULL, "|");
       strcpy(name, toks);
+
+      /* Divides the object location */
       toks = strtok(NULL, "|");
-      location=atol(toks);
-      
-      if(!(object=object_create(id))){
+      location = atol(toks);
+
+      /* Creates the object */
+      object = object_create(id);
+      if (!object) {
         fclose(file);
         return ERROR;
-      }else{
-        object_set_name(object, name);
-        object_set_location(object, location);
-        status=game_add_object(game, object);
-      }
+      }      
 
+      /* Sets the object attributes */
+      object_set_name(object, name);
+      object_set_location(object, location);
+
+      /* Adds the object to the game */
+      game_add_object(game, object);
+
+      /* Updates the space so it registers the object located in it */
       space = game_get_space(game, location);
-
-      if(space){
-        space_set_object_id(space, id);
+      if (space != NULL) {
+        space_set_objects(space, id);
       }
     }
   }
 
-  if (ferror(file)) {
-    status = ERROR;
-  }
-
   fclose(file);
-
   return status;
 }
 
-Status game_reader_load_player(Game* game, char* filename){
+Status game_reader_load_player(Game* game, char* filename) {
   FILE *file = NULL;
   char line[WORD_SIZE];
-  char name[WORD_SIZE] = "";
-  char *toks = NULL;      /*Tokens separated by strtok*/
-  Player *player = NULL;
+  char name[WORD_SIZE];
+  char *toks = NULL;
   Id id = NO_ID;
-  Status status= OK;
-
-  /*Checking if filename is not NULL*/
-  if(!(filename)){
+  Player *player = NULL;
+  Status status = OK;
+  
+  /* Control error */
+  if (!game || !filename) {
     return ERROR;
   }
-
-  /*Opening the data file in read mode and checking for errors*/
-  if(!(file=fopen(filename , "r"))){
+  
+  /* Opens the file */
+  if (!(file = fopen(filename, "r"))) {
     return ERROR;
   }
+  
+  /* Reads the file line by line */
+  while (fgets(line, WORD_SIZE, file)) {
 
-  /*Look for #p: which indicates the data is of type player*/
-  while(fgets(line, WORD_SIZE, file)){
-    if(strncmp("#p:", line , 3) == 0){
-      id = atol(strtok(line+3 , "|"));
-      toks = strtok(NULL , "|");
-      strcpy(name , toks);
-      
-      if(!(player=player_create(id))){
+    /* Checks if the line defines the player */
+    if (strncmp("#p:", line, 3) == 0) {
+
+      /* Divides the player id */
+      toks = strtok(line + 3, "|");
+      id = atol(toks);
+
+      /* Divides the player name */
+      toks = strtok(NULL, "|");
+      strcpy(name, toks);
+
+      /* Creates the player */
+      player = player_create(id);
+      if (!player) {
         fclose(file);
         return ERROR;
-      }else{
-        player_set_name(player, name);
-        status=game_add_player(game, player);
       }
+
+      /* Sets the player attributes */
+      player_set_name(player, name);
+
+      /* Adds the player to the game */
+      game_add_player(game, player);
     }
   }
   
-  if(ferror(file)){
-    status = ERROR;
-  }
-
   fclose(file);
-
   return status;
 }
